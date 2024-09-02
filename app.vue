3178bedebf41b513ccad75dcbad193647f0f7c78
martoast <alexmartos96@gmail.com>
2024-09-02 14:20:44 -0700

    use head

--- app.vue
+++ app.vue
@@ -3,3 +3,18 @@
     <Hero/>
   </div>
 </template>
+
+<script setup>
+
+useHead({
+  title: 'Testa Marketing',
+  description: 'Investigación de Mercados.',
+  ogDescription: 'Investigación de Mercados.',
+  htmlAttrs: {
+    lang: 'en'
+  },
+  meta: [
+    { name: 'description', content: 'Investigación de Mercados.' }
+  ]
+})
+</script>
