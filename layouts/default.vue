7700e8662ccadf0a27cc21439fe3f5ee494a989a
martoast <alexmartos96@gmail.com>
2024-09-05 09:57:42 -0700

    useServerSeoMeta

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,15 +8,21 @@
 
 <script setup>
   
-  definePageMeta({
-    title: 'Testa Marketing',
-    description: 'Investigación de Mercados.',
-    ogDescription: 'Investigación de Mercados.',
-    htmlAttrs: {
-      lang: 'en'
-    },
-    meta: [
-      { name: 'description', content: 'Investigación de Mercados.' }
-    ]
-  })
+  useServerSeoMeta({
+  title: 'Fullstack Labs - Software Development Experts',
+  ogTitle: 'Fullstack Labs - Software Development Experts',
+  author: 'Fullstack Labs',
+  charset: 'utf-8',
+  description: "Expert software development and consulting services for web, mobile, and emerging technologies.",
+  ogDescription: "Expert software development and consulting services for web, mobile, and emerging technologies.",
+  robots: 'index, follow',
+  googleSiteVerification: '',
+  appleMobileWebAppCapable: 'yes',
+  mobileWebAppCapable: 'yes',
+  ogImage: {
+    url: '/public/TESTA_LOGO CENTRAL COMPLETO_COLOR.svg'
+  },
+  ogLocaleAlternate: ['en_US'],
+  ogType: 'website'
+})
   </script>
