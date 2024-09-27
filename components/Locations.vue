2d02e3bcfd57bcb7f71310e3946bd2daccb8c664
martoast <alexmartos96@gmail.com>
2024-09-27 09:39:48 -0700

    update directions

--- components/Locations.vue
+++ components/Locations.vue
@@ -12,8 +12,8 @@
               <div class="rounded-2xl bg-gray-50 p-10">
                 <h3 class="text-base font-semibold leading-7 text-gray-900">Tijuana B.C.</h3>
                 <address class="mt-3 space-y-1 text-sm not-italic leading-6 text-gray-600">
-                  <p>Sach - Edificio Torella Corporativo</p>
-                  <p>Blvd. Agua Caliente 9955-Piso 5</p>
+                  <p>Sach - Edificio Torella Corporativo,</p>
+                  <p>Blvd. Agua Caliente 9955-Piso 5,</p>
                   <p>Calete, 22044 Tijuana, B.C.</p>
                   <p>+52 (664) 889-5909</p>
                   <p><a href="mailto:info@testamarketing.com" class="text-primary">info@testamarketing.com</a></p>
@@ -33,7 +33,8 @@
               <div class="rounded-2xl bg-gray-50 p-10">
                 <h3 class="text-base font-semibold leading-7 text-gray-900">Ciudad de México</h3>
                 <address class="mt-3 space-y-1 text-sm not-italic leading-6 text-gray-600">
-                  <p>Calz. Gral. Mariano Escobedo 476-piso 12, int. 1200-B,</p>
+                  <p>Calz. Gral. Mariano Escobedo,</p>
+                  <p>476-piso 12, int. 1200-B,</p>
                   <p>Chapultepec Morales, Verónica Anzúres, Miguel Hidalgo, 11592 Miguel Hidalgo,</p>
                   <p>Ciudad de México</p>
                   <p>+52 (559) 417-5263</p>
@@ -43,7 +44,7 @@
               <div class="rounded-2xl bg-gray-50 p-10">
                 <h3 class="text-base font-semibold leading-7 text-gray-900">España, Barcelona</h3>
                 <address class="mt-3 space-y-1 text-sm not-italic leading-6 text-gray-600">
-                  <p>Av. Diagonal, 211 – PL.27 LOOM</p>
+                  <p>Av. Diagonal, 211 – PL.27 LOOM,</p>
                   <p>08018 - Barcelona</p>
                   <p>+34 670 35-3214</p>
                   <p><a href="mailto:josepmaria@testamarketing.com" class="text-primary">josepmaria@testamarketing.com</a></p>
