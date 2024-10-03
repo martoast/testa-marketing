7d15ec6368a4ef77c66c119da11b3d57a066a8c1
martoast <alexmartos96@gmail.com>
2024-10-02 23:12:38 -0700

    new link to og url

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,7 +20,7 @@
   appleMobileWebAppCapable: 'yes',
   mobileWebAppCapable: 'yes',
   ogImage: 'https://scontent.ftij3-1.fna.fbcdn.net/v/t39.30808-6/313407376_495827589249462_8953498923463885895_n.png?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=crb2e41u8poQ7kNvgEJdI3w&_nc_ht=scontent.ftij3-1.fna&_nc_gid=A3VIryFLkdG5amSTvc1WwgB&oh=00_AYARAg9wncCEfAy8O0knZaSRwp6qDVnTZ5Fr3MDAZKpMiA&oe=670374E6',
-  ogUrl: 'https://testa-marketing.netlify.app/',
+  ogUrl: 'https://testamarketing.com/',
   ogLocaleAlternate: ['en_US'],
   ogType: 'website'
 })
